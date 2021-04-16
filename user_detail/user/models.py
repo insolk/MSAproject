c34cdf321a4021590dd6cf6bf1ba0d069d41a493
Jaewoo <jaewoo.jeongg@gmail.com>
2021-04-16 23:14:36 +0900

    user_detail ver2

--- user_detail/user/models.py
+++ user_detail/user/models.py
@@ -8,8 +8,8 @@ class user(models.Model):
     user_gender = models.CharField(max_length=20)
     user_nickname = models.CharField(max_length=20)
     user_created = models.DateTimeField('Create Date', auto_now_add=True)
-    user_birthdate = models.DateTimeField(max_length=9)
-    user_caution = models.IntegerField(blank=True, null=True)
+    user_birthdate = models.DateTimeField(max_length=9) # char? date?
+    user_caution = models.IntegerField(blank=True, null=True) 
     user_sell_count = models.IntegerField(blank=True, null=True)
 
     class Meta:
--- user_detail/user/views.py
+++ user_detail/user/views.py
@@ -8,10 +8,6 @@ from django.shortcuts import render
 class userDV(DetailView):
     model = user
     template_name = "user/index.html"
-    # context_object_name='user'
 
-    # def get_context_data(self,**kwargs):
-    #     context = super().get_context_data()
-    #     return context
 
 
