e8bb48dd80bbca12100d40c975cd9331a4e4f036
youminji62 <youminji62@gmail.com>
2021-04-17 18:19:49 +0900

    complete item_detail

--- item_detail/items/urls.py
+++ item_detail/items/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name = 'items'
+app_name='items'
 
 urlpatterns = [
-    #path('',views.item_detail, name='items_detail'),
-    path('<int:pk>/', views.item_detail, name='item_detail'),
-    path('<int:pk>/comment', views.comment_detail, name='comment_detail'),
-    #path('<int:pk>/comment/delete', views.comment_update, name='comment_update')
+    path('admin/', admin.site.urls),
+    path('items/<int:product_id>/', views.item_detail, name='items_detail'),
+    path('items/<int:product_id>/comment', views.comment_insert, name='comment_insert'),
+    path('items/<int:product_id>/comment/<int:comment_id>', views.comment_detail, name='comment_detail'),
+    path('items/<int:product_id>/comment/<int:comment_id>/update',views.comment_update, name='comment_update')
 ]
